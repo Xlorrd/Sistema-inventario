32315111551213e90e9e5137f829e678cddb3017
Alex simbaña <alx.simb@hotmail.com>
2023-05-24 14:37:44 -0500

    Version 3.3
    Se agregan comentarios en la clase Crud_tbl_activo

--- Controlador/Crud_tbl_activo.cs
+++ Controlador/Crud_tbl_activo.cs
@@ -8,21 +8,28 @@ namespace Controlador
 {
    public class Crud_tbl_activo
     {
+        //Clase publica instanciando al modelo de base de datos bdactivos ubicado
+        //en la capa modelo ne negocio hace referencia a llamada a las tablas y atributos de las mismas
         public static DbActivosDataContext dc = new DbActivosDataContext();
 
+        //metodo creado para listar  los activos con estado ="activo"
         public static List<listar_activoResult> Listar_Activo()
         {
             var list = dc.listar_activo();
             return list.ToList();
         }
-
+        //metodo creado para mostrar los activos con estado activo e inactivo 
         public static List<listar_all_activoResult> Listar_all_Activo()
         {
             var list = dc.listar_all_activo();
             return list.ToList();
         }
+
+        //metodo creado para insertar datos en la tabla tbl_activo
         public static void Insertar_Activo(tbl_activo ta)
         {
+            //utilizamos la funcion try para controlar erroes en ejecucion del programa si llegase a ocurrir
+            //    un error el programa  no se cerrara y continuara mostrando el error desisgado en la llamada exeption ex
             try
             {
                 var insert = dc.insertar_activo(
@@ -46,6 +53,8 @@ namespace Controlador
             }
 
         }
+        //Metodo creado que necesita un parametro para realiar una busqueda en la tabla tbl_activo 
+           
         public static List<buscaMasiva_activoResult> Buscar_Activo(string descripcion)
         {
 
@@ -53,7 +62,7 @@ namespace Controlador
             return list.ToList();
         }
 
-        //Lista de la ventana modal para capturar un activos con disponiblilidad "Libre"
+        //Lista de la ventana modal para capturar un activos con disponiblilidad "Libre" ----> "no utilizado "
         public static List<Activos_disponiblesResult> Activo_disponible()
         {
             var list = dc.Activos_disponibles();
@@ -61,6 +70,7 @@ namespace Controlador
         }
         // Buscar activos con estado 0 ("Disponibles")
 
+            //metodo para buscar activos disponibles- ---> " no utulizado "
         public static List<buscar_AcDisponibleResult> Buscar_ActivoDiponible( string descripcion)
         {
             var list = dc.buscar_AcDisponible(descripcion);
---
+++ Vista/obj/Release/Package/PackageTmp/Rol.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Rol.aspx.cs" Inherits="Presentacion.Rol" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+     <h2> <asp:Label ID="lbl_titulo" runat="server" Text=""></asp:Label>
+            </h2> 
+            <p>
+                 <asp:TextBox class="form-control" ID="txt_buscar" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                <asp:Button ID="bt_buscar" class="btn btn-primary" runat="server" Text="Buscar" OnClick="bt_buscar_Click"  />
+            </p>
+            <p>
+                <asp:GridView ID="grid_rol" CssClass="table  table-sm table-bordered table-condensed table-responsive-sm table-hover " runat="server" OnSelectedIndexChanged="grid_rol_SelectedIndexChanged" AllowPaging="True" OnPageIndexChanging="grid_rol_PageIndexChanging" >
+                 <PagerSettings Mode="NumericFirstLast" PageButtonCount="5" FirstPageText="Primero" LastPageText="Último"/>
+                               <PagerStyle CssClass="pagination-ys" HorizontalAlign="Center" />
+                </asp:GridView>
+            </p>
+            <p>
+                <asp:Button class="btn btn-primary"  ID="btn_nuevo" runat="server" Text="Ingresar Nuevo" OnClick="btn_nuevo_Click" />
+            </p>
+          <asp:Label  ID="lbl_descripcion" runat="server" Text="Descripción Rol"></asp:Label>
+          <asp:TextBox class="form-control" ID="txt_descripcion" runat="server"></asp:TextBox>
+        <asp:Label  ID="lbl_estado" runat="server" Text="Estado Rol"></asp:Label>
+          <asp:TextBox class="form-control" ID="txt_estado" runat="server"></asp:TextBox>
+
+    
+      <asp:Button class="btn btn-primary" ID="btn_guardar" runat="server" Text="Guardar" OnClick="btn_guardar_Click" />
+      <asp:Button class="btn btn-primary" ID="btn_editar" runat="server" Text="Guardar" OnClick="btn_editar_Click"  />
+
+</asp:Content>
